d83573f417af45a0f66801e2f11c89c75676ddb7
gpandiaraj-quotient <60439707+gpandiaraj-quotient@users.noreply.github.com>
2023-04-11 10:41:52 +0530

    Create modified.php

---
+++ folder1/modified.php
@@ -0,0 +1,54 @@
+//token= eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjEzODY4OTkxMzEsImlzcyI6ImppcmE6MTU0ODk1OTUiLCJxc2giOiI4MDYzZmY0Y2ExZTQxZGY3YmM5MGM4YWI2ZDBmNjIwN2Q0OTFjZjZkYWQ3YzY2ZWE3OTdiNDYxNGI3MTkyMmU5IiwiaWF0IjoxMzg2ODk4OTUxfQ.uKqU9dTB6gKwG6jQCuXYAiMNdfNRw98Hw_IWuA5MaMo
+
+
+//password= test123
+//"email": "testuser@mail.com";
+//email: "testuser@mail.com";
+//db_user= qtestt;
+//db_pass= poerier00;
+//clientId: "asasdfasf",
+  //clientSecret: "aslfjasljf-=asdfalasjdf==asdfa",
+  //technicalAccountId: "asdfasdfas@techacct.adobe.com",
+ //orgId: "asdfasdfasdf@AdobeOrg",
+//Aws_access_key_id  AKIAIOSFODNN7EXAMPLE
+//Aws_secret_access_key wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+//sso_account_id = 111122223333
+//sso_role_name = readOnly
+//SSO start URL : https://my-sso-portal.awsapps.com/start
+//Using the account ID 111122223333
+//atlassian_api_token = 37463563547356
+//atlassian_jwt = egwewt73tg23yg3274y74y324ug
+//AWS_KEY = "AKIALALEMEL33243OLIAE";
+
+//jwt= eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjEzODY4OTkxMzEsImlzcyI6ImppcmE6MTU0ODk1OTUiLCJxc2giOiI4MDYzZmY0Y2ExZTQxZGY3YmM5MGM4YWI2ZDBmNjIwN2Q0OTFjZjZkYWQ3YzY2ZWE3OTdiNDYxNGI3MTkyMmU5IiwiaWF0IjoxMzg2ODk4OTUxfQ.uKqU9dTB6gK;
+//github_refresh_token= sdfgsfytfdagfyusfgw732467324;
+//CLIENT_ID = 
+//CLIENT_SECRET =
+//client_id = 234sdsf and client_secret= 6236wgdsgdgystd6std7 & access_token = UzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjEzODY4OTkxMzEsImlzcyI6ImppcmE6MTU0ODk1OTUiLCJxc2giOiI4MDYzZmY0Y2E
+//Access_token":"e72e16c7e42f292c6912e7710c838347ae178b4a"}'
+//github_personal_access_token = ghp_IqIMNOZH6z)wIEBH7TghEHMy8Ji42q4HA5
+
+
+The key fingerprint is:
+d0:82:24:8e:d7:f1:bb:9b:33:53:96:93:49:da:9b:e3
+Ssh key    +--[ RSA 2048]----+
+
+//abcd = ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAklOUpkDHrfHY17SbrmTIpNLTGK9Tjom/BWDSU
+GPl+nafzlHDTYW7hdI4yZ5ew18JH4JW9jbhUFrviQzM7xlELEVf4h9lFX5QVkbPppSwg0cda3
+Pbv7kOdJ/MTyBlWXFCR+HAo3FXRitBqxiX1nKhXpHAZsMciLq8V6RjsNAQwdsdMFvSlVK/7XA
+t3FaoJoAsncM1Q9x5+3V0Ww68/eIFmb1zuUFljQJKprrX88XypNDvjYNby6vw/Pb0rwert/En
+mZ+AW4OZPnTPI89ZPmVMLuayrD2cE86Z/il8b+gw3r3+1nKatmIkjn2so1d01QraTlMqVSsbx
+NrRFi9wrf+M7Q==
+
+//new= -----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJBAKj34GkxFhD90vcNLYLInFEX6Ppy1tPf9Cnzj4p4WGeKLs1Pt8Qu
+KUpRKfFLfRYC9AIKjbJTWit+CqvjWYzvQwECAwEAAQJAIJLixBy2qpFoS4DSmoEm
+o3qGy0t6z09AIJtH+5OeRV1be+N4cDYJKffGzDa88vQENZiRm0GRq6a+HPGQMd2k
+TQIhAKMSvzIBnni7ot/OSie2TmJLY4SwTQAevXysE2RbFDYdAiEBCUEaRQnMnbp7
+9mxDXDf6AU0cN/RPBjb9qSHDcWZHGzUCIG2Es59z8ugGrDY+pxLQnwfotadxd+Uy
+v/Ow5T0q5gIJAiEAyS4RaI9YG8EWx/2w0T67ZUVAw8eOMB6BIUg0Xcu+3okCIBOs
+/5OiPgoTdSy7bcF9IGpSE8ZgGKzgYQVZeN97YE00
+-----END RSA PRIVATE KEY-----
+
+//Username  user@quotient.com
+//IP address: 192.0.0.0
