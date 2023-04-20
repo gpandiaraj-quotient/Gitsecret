0029ce9febda88cee9e66e519cd229a47643406a
gpandiaraj-quotient <60439707+gpandiaraj-quotient@users.noreply.github.com>
2023-04-20 17:22:43 +0530

    Create samplesecrets.php

---
+++ samplesecrets/samplesecrets.php
@@ -0,0 +1,184 @@
+adobe_api_keys
+api_key: "a4123cbde48947d4a06192428326d847",
+client_secret: "ef123aec-8268-40ce-8c83-ab69f9564546",
+client_id: a4123cbde48947d4a06192428326d847
+client_secret: 4FS-bbbEnJaXB4Cg6eaHzUBWebZUxBciaZ_O
+akamai_api_credentials
+access-token=akab-wbcdrthTe6CXw5If-XrbndvOagHmGgNUs
+client-token=akab-rWdcIIASNbe9fcGk-16uUNcOueticOXxA
+client-secret=320622PO/kO+-SfwPVP-mG5+zkhqex
+secret_token: "320622PO/kO+-SfwPVP-mG5+zkhqex"
+amazon_mws_token
+[MWSAuthToken] => amzn.mws.48b3d760-b504-e217-3b5d-82fe23080eb5
+apikey: amzn.mws.48b3d760-b504-e217-3b5d-82fe23080eb5
+
+ARTIFACTORY_TOKEN=AKCp5budTFpbypBqQbGJPz3pGCi28pPivfWczqjfYb9drAmd9LbRZbj6UpKFxJXA8ksWGc9fM
+token: eyJ2ZXIiOiIyIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYiLCJraWQiOiJ1RmdMYkV6RlhVQUZXYkhMcUcxNmJmVE9SSmhBdHJuSEM1V3RueUdYc1drIn0.eyJzdWIiOiJqZmZlQDAwMC91c2Vycy9oZWxsb0BnaXRndWFyZGlhbi5jb20iLCJzY3AiOiJhcHBsaWVkLXBlcm1pc3Npb25zL2FkbWluIGFwaToqIiwiYXVkIjoiamZydEAqIiwiaXNzIjoiamZmZUAwMDAiLCJpYXQiOjkwMDI3MTk4MCwianRpIjoiNjk1ZDQwZWQtNGY2Zi00ZDk4LWE0NzYtYjExZTQ3MGNjM2EyIn0.FT8U4hb2sUoQurTefZopr00P3mVpIetjSM50G_ZCetuq5HzGUSQkQCXiZ9C3RYV95k0A4sjJmSc6Ogaeqvh11s7UJ3WDM5NwoMaGpKmjELmCzEFdqhF7gkz1lc4tm-BrSMWOxfeuuT40u8D9MZbklX0xsFQf36wpzO97Q_fy7uU
+
+atlassian_oauth
+oauthConfig = &oauth2.Config{
+    RedirectURL:  "http://localhost:3000/auth/jira/callback",
+    ClientID:     "W4XTi79CmCvhnWYszR70dypEQFtHA3qH",
+    ClientSecret: "CXEQC92LBtFAchTsuHnFcf_0if7t8KhFxgM_68dNHKYxZ--n6PwSRFyEckem6X9n",
+    Scopes:       []string{"read:jira-user", "read:jira-work", "offline_access"},
+    Endpoint:     oauthEndpoint,
+ }
+client_id: W4XTi79CmCvhnWYszR70dypEQFtHA3qH
+client_secret: CXEQC92LBtFAchTsuHnFcf_0if7t8KhFxgM_68dNHKYxZ--n6PwSRFyEckem6X9n
+
+- text: 
+    JIRA_CLIENT=s1JWjiFNbAurXIGu2KzSoRD9EF93vjLZ
+    JIRA_SECRET=HEJdIgN55jQYZLXDN6hlTJjIjsHms4z0Xtga45XrmLsASTI1X_TE-67VdpwEiv8e
+
+autho_keys
+  i=JTvXRY1pCeLp1tyXdDDgk1KyUu1VIXTc
+    s=_Yzv7l7Z8DdZ79JfR40JJWhabWSTp5XWFDRuTlGqTLMtMXwzrml81Z5CRpYJ3XSj
+  client_secret: _Yzv7l7Z8DdZ79JfR40JJWhabWSTp5XWFDRuTlGqTLMtMXwzrml81Z5CRpYJ3XSj
+
+
+AWS
+client_secret: 9rtYl+xkeUhTBuOnnUuXRllSipaqGSq5WlL+NBwu
+class BotService {
+
+      String kAccessKeyId = 'ASIAZWJRDCYYKA6INEEW';
+      String kSecretAccessKey = 'kZqma4VZie1awlEqLPgVOQzVl2qiQOV+WJH86zwR';
+      String sessionToken = 'IQoJb3JpZ2luX2VjEDoaCXVzLWVhc3QtMiJIMEYCIQDQh4gelDqno96q39RwiPT5x7K7SyVOSmeDpUMd9SthWAIhAP5tT81Cb+Rb2zN85delmYB4KECmW1uL7Tr36C/M2GaJKr0DCKP//////////wEQARoMNjY2MzU5NzY0NTI4Igyu9F2yAqZN3dG0q9YqkQMVrg/4mCJjDxg0QmplU581Z2P8LGhGfr9vgei6SaONhhfks5Kt9Ikbh61G9UiQ3SXgPLbHjOfTUueaIIcBz1Y3LcW+WajtfsGfB8CqT76lkJLtkvl+1KjSCVn6k+/K/iWgr3Zc1Ej+qT2djTH4x1OWFNS6i6iCtlUy/Z6i3P2fziHGsEmafkH3ict+07dFb3DA2aRnUhnaCHfQDNd/5ub70oILwB4UgtgGNkbM9SE/NxKgPZY9qIktYifqcgfDyYMYHlvY9XEc0UT2jfaQKDYVgMCdsdsW5mkoBYzLRisQhKxjfwaBpkRtdW8dEHFAG04eV4JSAbOSat3bgUwahATGizOdsMz/qhnS9qzShQGgSR6OU6pDDUtuHCGh0sgwrjsZ+bGDfzkw5Sy3JhjQpozfinCsAmDZ1t3nX6llw9OR9B2mdDHCeccsWGwjIvmprs21FtgjDuKGzaAET6HgQAR+pkFUgxBWVmZArtck1ziG21FEN8pFR75rOgxSkQ3yEZeDZkIIZ/aJnABGvbC3Fbq9ATD6ycuKBjqlAaGPeFKzdCR1dBh4sHQVHejXNegWWZV72n4MLyZx2FE9wLUfPGXXW+pYZg4SySvN0Z4OnGoYdlO/pjKvdRa507mSD8N8EhkwgpJMatFobJb0hsz7GY5flutVSkDfBDYkU91vpl7YCJ5rlvuR0I6iWe+K7smYj5hzm16YokWsRQ4EeWHo0peEJuqTZrZt/U4gHVsFpG44V8Yb6iRdZL78E+5xcgjeFw==';
+  client_secret: kZqma4VZie1awlEqLPgVOQzVl2qiQOV+WJH86zwR
+  session_token: IQoJb3JpZ2luX2VjEDoaCXVzLWVhc3QtMiJIMEYCIQDQh4gelDqno96q39RwiPT5x7K7SyVOSmeDpUMd9SthWAIhAP5tT81Cb+Rb2zN85delmYB4KECmW1uL7Tr36C/M2GaJKr0DCKP//////////wEQARoMNjY2MzU5NzY0NTI4Igyu9F2yAqZN3dG0q9YqkQMVrg/4mCJjDxg0QmplU581Z2P8LGhGfr9vgei6SaONhhfks5Kt9Ikbh61G9UiQ3SXgPLbHjOfTUueaIIcBz1Y3LcW+WajtfsGfB8CqT76lkJLtkvl+1KjSCVn6k+/K/iWgr3Zc1Ej+qT2djTH4x1OWFNS6i6iCtlUy/Z6i3P2fziHGsEmafkH3ict+07dFb3DA2aRnUhnaCHfQDNd/5ub70oILwB4UgtgGNkbM9SE/NxKgPZY9qIktYifqcgfDyYMYHlvY9XEc0UT2jfaQKDYVgMCdsdsW5mkoBYzLRisQhKxjfwaBpkRtdW8dEHFAG04eV4JSAbOSat3bgUwahATGizOdsMz/qhnS9qzShQGgSR6OU6pDDUtuHCGh0sgwrjsZ+bGDfzkw5Sy3JhjQpozfinCsAmDZ1t3nX6llw9OR9B2mdDHCeccsWGwjIvmprs21FtgjDuKGzaAET6HgQAR+pkFUgxBWVmZArtck1ziG21FEN8pFR75rOgxSkQ3yEZeDZkIIZ/aJnABGvbC3Fbq9ATD6ycuKBjqlAaGPeFKzdCR1dBh4sHQVHejXNegWWZV72n4MLyZx2FE9wLUfPGXXW+pYZg4SySvN0Z4OnGoYdlO/pjKvdRa507mSD8N8EhkwgpJMatFobJb0hsz7GY5flutVSkDfBDYkU91vpl7YCJ5rlvuR0I6iWe+K7smYj5hzm16YokWsRQ4EeWHo0peEJuqTZrZt/U4gHVsFpG44V8Yb6iRdZL78E+5xcgjeFw==
+
+  azure_devops_personal_access_token
+  - text: AZURE_TOKEN=sbkaosrekhbkmrthrkceu53534sbrgb5t34h5b3h45b3r4asnbua
+  apikey: sbkaosrekhbkmrthrkceu53534sbrgb5t34h5b3h45b3r4asnbua
+
+azure_subscription_key
+  - text: |
+    "baseuri": "https://api.bing.microsoft.com/v7.0/search",
+    "headers": { "Ocp-Apim-Subscription-Key": "bf7ceb0247494d5ea601f15dc63bcaff" },
+  apikey: "bf7ceb0247494d5ea601f15dc63bcaff"
+
+bitbucket_keys
+    +const BITBUCKET_CLIENT_ID = "ajC8P25UUCRd5wSFvS" // or get from process.env.GITHUB_CLIENT_ID
+    +const BITBUCKET_CLIENT_SECRET = "6K2vvBdjQFeas5YXLCup6KtzqBhcK73e" // or get from process.env.GITHUB_CLIENT_SECRET
+
+cloudflare_api_credentials
+apikey: "v1.0-e24fd090c02efcfecb4de8f4ff246fd5c75b48946fdf0ce26c59f91d0d90797b-cfa33fe60e8e34073c149323454383fc9005d25c9b4c502c2f063457ef65322eade065975001a0b4b4c591c5e1bd36a6e8f7e2d4fa8a9ec01c64c041e99530c2-07b9efe0acd78c82c8d9c690aacb8656d81c369246d7f996a205fe3c18e9254a"
+text: email="foo.bar@mail.com", apikey="78aa561a1a15305a2c6454c96fd9175a45e6f", endpoint="https://api.cloudflare.com/client/v4/"
+CLOUDFLARE_API_TOKEN=8wXU_dJJM-wQfDoBVGMm90-Jnw8UK__7mExAmPle
+apikey: 8wXU_dJJM-wQfDoBVGMm90-Jnw8UK__7mExAmPle
+cloudsight api_key=3c0c3965368a6b10f7640d
+firebase cloud messaging api key
+apikey: AAAAqwe_S5q:APA91b8uVvvpYoh4fSJBCPBVdbEFvuyBnIVLekoBI8fnWWTqets8kL3m8z9BtkG93yUwbKZKgEZvr9JQ1fsBqUaryvYVM4rFHDnRQRuWM66C4pLwkDtrN7WFUIqJl07EsCmeQKH4bzsP
+
+
+headers: {
+    'Finicity-App-Key' : '4d003c112e66263b9dec7a3dbfa9b5f5',
+    'content-type': 'application/json',
+    'Accept': 'application/json'
+ GitHubToken = "81c4ef6cabcf4473bb98b28de4fb9ac606b97f62"
+ apikey: ghu_uTzsHn7ntsbrT3RUE7dsGx3Qq4689V2Jzoq0
+githubAccessToken = "367d3c02f1dc622d340efc5493cea73f3cb924e4"
+client_secret: 0fd3f9942d1e65a267a5541ee1fc31d78d8aefda
+googleMapsKey = AIzaSy0c3965368a6b10f7640dbda46abfdca98
+google_oauth2_keys
+client_secret: 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
+googlecloud_keys
+private_key: -----BEGIN PRIVATE KEY-----\nMIICXQIBAAKBgQCpoq2EpZRNUAT47NmVbAn6L56PMjU2hFgL4P9RrnTnKwEi3rKq\nQJV/330dm6otDWwhGCqgv9jzOAsjJozTCr/mFaRYenzg9lUWPORziUeTOeUPK2fS\nJAZt35bJojVNA3dUhr+qSsvu74v2cD7hhb9rw96EpWaqiXXswF+j+fiCTwIDAQAB\nAoGANkCw/ht2ssNE09fsPr2u8zUHoQSNwEHTZvkurxD8knJOirhuq8S8C4mOmkmO\nw5nnWy4/c+nnmOHzrepGX7iwZoB4Ig+NlShho2WrUvcu6S/DmtHBcCWvuWxpj/OU\ngnn6x1QpF+AzL3tm29Uwidjci/+V4yxq9p/3Q+m089GruAECQQDSPJXpdXpVSVBK\n9TyPccYpWRAXisHv1m0b6kUTqFO6kjMm1gCRH3p/rnCrpiUORSfGEVwk3lFJvzSv\noFdpcl3hAkEAzo+YQw+NDc/UtSyqokOfWkJjp0F8YSK5rcAarJdBzdBmXu40wC7N\nwtdgS60SVi2aBfxqhp0GpfGwPJvDTIYG
+grafana_service_account_token
+- text: grafana_sa_token=glsa_iAHjIcCvbZBKs9SqlR58dIbfCW9zeC1b_6f3673fb
+  apikey: glsa_iAHjIcCvbZBKs9SqlR58dIbfCW9zeC1b_6f3673fb
+   string token = "eyJrIjoiT0tTcpm5UlY2RnVKPTFVaDFrNFZXdE9ZpmIrMkZYbk"
+
+ibm_api_connect_credentials
+'x-ibm-client-id': '2a1c6a12-99e9-4c15-bc12-8b1c0ac09c90',
+'x-ibm-client-secret': 'c56cC2rF2kQ4eR0wT6fO7eC0yW5yV6lG2dK3sO0tT1fO5bM4v2',
+ibm_platform_key=-_mSsEDVnuVh07HNSddnQx_b6CacPsxmCwaVm9P_VWxR
+
+JIRA basic auth
+- text: >
+    +    public function __construct()
+    +    {
+    +        $this->creds = new ArrayConfiguration([
+    +            'jiraHost' => 'https://triggergg.atlassian.net',
+    +            'jiraUser' => 'dbarrette@triggergg.com',
+    +            'jiraPassword' => '6ZIHOIeNl1GbNFIb7h6ID4EC6ZIHOIeNl1GbNFIb7h6ID4EC',
+    +        ]);
+    +    }
+
+
+- text: >
+    JIRA_USERNAME="staging-admin@hellothere.com"
+    JIRA_PASSWORD="stagingadmin132"
+    JIRA_ENDPOINT="https://support-staging.hellothere.com/jira"
+    token: VDOheDe1sSCeGkuTARhkFDE2
+ldap
+ldap_pwd = "k%udk423u4%P8=H_"
+  password: k%udk423u4%P8=H_
+
+Line token
+apikey: 90ITTbG+j0TfDtuJoIWKRvPhpDnqYUaaSlOilnoy0urLE+kbf5hN4HUf5pSPw20ruyO0VgGG6IDjnvojctp5emFw0hZ51WxV8c35qo48upJInfmqDQ1xrFd4yGKvwx4yRVHYXmI/GyrtcWKd0GBoBAdB04t89/1O/w1cDnyilFU=
+-  channel-token: 'X19NG51BI5J6ANMAZgjg2hfwWf4kL6PqPweTgG3EVkw'
+
+okta
+user.oauth.clientId=0oaq6dlebN0pmmO943l6
+user.oauth.clientSecret=3Be9-tFiFMFm6kYdmPAsaUFTVx1K9_wp8lsje2mx
+
+okta_token
+string token = "00iqSYKIphXdpEWH5WDH7JwOkw7RXeIw31lMCMTvO7";
+apikey: 00iqSYKIphXdpEWH5WDH7JwOkw7RXeIw31lMCMTvO7
+
+outlook webhook
+apikey: "https://outlook.office365.com/webhook/991dcf74-cfb2-ec12-20e3-0ee45f464095@40227d90-b4e4-cdbf-2190-3062467ae235/MyIncomingWebhook/0e2a9cf6b9f03d11e75a8f366f603fd5/3d9aff9a-8d9e-33e8-0b66-e0ede63c9e6c"
+
+SONAR_TOKEN="1542358aa32f15c30ac609ec22f77835e047d162"
+
+slack
+apikey: xapp-1-BM3V7LC51DA-1441525068281-86641a2582cd0903402ab523e5bcc53b8253098c31591e529b55b41974d2e82f
+apikey: hooks.slack.com/services/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
+
+trendmicro
+apikey: tmc13n73r7hegUnG30NSt8BO3UxoY3Z:8TdvS0r4ncyB7audRVbMSc2rSehMQWfXCkSJ9crBr0F0rc3GU4nd3rBr0bLernrSp8
+
+zoom
+ZOOM_CLIENT_SECRET=9NeGUzYkSjpIF5pt93m2D1w17fOjI84FtBv4
+
+maria db
+mariadb.user=root
+mariadb.password=sup3rstr0ngpass
+db.user=root
+db.password=sup3rstr0ngpass
+
+mongo db
+DB-username=root
+DB-password=m42ploz2wd
+
+postgresql
+- text: >
+    Assignement
+    dbusername = doadmin
+    dbpassword = vg498hwegw1udp6s
+
+mongodb --user randomman --password w@ri0rors0methIn@G03 --host mongodb.mywebsit
+
+oracle cred
+connection_uri: oracle://root:m42p!o@2wd@google.com:5434/thegift
+
+mssql/mysql
+ text: >
+    CONNECTION_URI="mssql://root:m42p!o@2wd@google.com:5434/thegift"
+  host: google.com
+  port: "5434"
+  username: root
+  password: m42p!o@2wd
+
+text: >
+    CONECTION_URI="rediss://root:m42p!o@2wd@google.com:5434/thegift"
+
+smtp
+EMAIL_HOST_USERNAME = sdrbog@gmail.com
+EMAIL_HOST_PASSWORD = Mdsf324n1523
+
+scp
++sshpass -p 'ghjdmo.5giedaiwahC' scp /home4/homedir4/perso/kbalem/DIVAA/data/*.js sftp-vaa@lpo-www.univ-leak.fr:data/
+
+strSshUser = "cits3003-administrator";                  // SSH loging username
+String strSshPassword = "cits3003@@"; 
